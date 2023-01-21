2fd160e992740e9b90be4263164a609cff9c6daa
Rix <rixwoodling@gmail.com>
2023-01-20 22:36:29 -0800

    Update hsinchu.py

--- hsinchu.py
+++ hsinchu.py
@@ -6,4 +6,22 @@ import random
 piglow.auto_update = True
 piglow.all( 0 )
 
+def colorful_raindrops():
+    count = 0
+    leds = list(range( 0,18 ))
+    random.shuffle( leds )
+    while ( count < 3 ):
+        for i in leds:
+            piglow.led( i, 100 )
+            sleep( 0.1 )
+        random.shuffle( leds )
+        for i in leds:
+            piglow.led( i, 0 )
+            sleep( 0.1 )
+        count += 1
 
+if __name__ == '__main__':
+    while True:
+        colorful_raindrops()
+
+#        
